5b302cc618b9945e79efa5e68f3c481f72602945
Samir Maharjan <samir84753@gmail.com>
2020-02-11 20:17:03 +0545

    email script

---
+++ sendmail.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "$USER"
+echo "Enter recepient email:"
+read remail
+echo "Enter title of the email:"
+read subject
+echo "Enter email message:"
+read body
+
+cat <<EOF > email.txt
+Subject: $subject
+$body
+EOF
+echo "Sending email to $remail"
+sendmail "$remail" < email.txt
+
